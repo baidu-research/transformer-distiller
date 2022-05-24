f8429ad264aa22e7c2e4c61571250b211ac10741
jiaji-huang <jiaji-huang@outlook.com>
2022-05-24 10:06:45 -0700

    minor fix

--- src/data/scripts/mnli_hdf5.py
+++ src/data/scripts/mnli_hdf5.py
@@ -7,5 +7,10 @@ with open('/tmp/mnli_train.txt', 'w') as f:
 #python h5_data.py \
 #    --data_files /tmp/mnli_train.txt \
 #    --tokenizer_name bert-base-uncased \
-#    --dump_file /mnt/data/jiajihuang/model_compression/eval_data/mnli_ragged.hdf5 \
+#    --dump_file /path/to/eval_data/mnli_ragged.hdf5 \
 #    --ragged
+
+#python h5_data.py \
+#    --data_files /tmp/mnli_train.txt \
+#    --tokenizer_name bert-base-uncased \
+#    --dump_file /path/to/eval_data/mnli.hdf5 \
